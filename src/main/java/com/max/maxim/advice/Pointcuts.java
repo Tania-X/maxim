4da2850e1b8da78ac87f872b17affec47e0ecc16
max <13126565350@163.com>
2023-08-30 18:13:25 +0800

    新增过滤器，获取Http请求携带的用户密码信息。另外，注意PointCuts类的变更。

--- src/main/java/com/max/maxim/advice/Pointcuts.java
+++ src/main/java/com/max/maxim/advice/Pointcuts.java
@@ -1,5 +1,6 @@
 package com.max.maxim.advice;
 
+import jakarta.servlet.FilterConfig;
 import org.aspectj.lang.annotation.Pointcut;
 
 /**
@@ -7,7 +8,15 @@ import org.aspectj.lang.annotation.Pointcut;
  */
 public class Pointcuts {
 
-  @Pointcut("execution(public * com.max.maxim.controller.*.*(..)) || execution(public * com.max.maxim.user.*.*(..))")
+  /**
+   * all the filters are related to {@link org.springframework.web.filter.GenericFilterBean#init(FilterConfig)}
+   * while init method is modified by 'final', dynamic proxies can't be applied to it.
+   * we have to ignore it from pointcut, otherwise this exception occurs:
+   * Cannot invoke "org.apache.commons.logging.Log.isDebugEnabled()" because "this.logger" is null
+   */
+  @Pointcut("execution(public * com.max.maxim.controller.*.*(..))"
+      + " || execution(public * com.max.maxim.user.*.*(..))"
+      + " && !execution(public * com.max.maxim.user.*Filter.*(..))")
   public void httpLog() {
 
   }
---
+++ src/main/java/com/max/maxim/user/CustomLoginFilter.java
@@ -0,0 +1,75 @@
+package com.max.maxim.user;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.max.maxim.constant.MaximConstant;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationServiceException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.util.ObjectUtils;
+
+@Slf4j
+public class CustomLoginFilter extends UsernamePasswordAuthenticationFilter {
+
+  private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+  public CustomLoginFilter(AuthenticationManager authenticationManager,
+      CustomAuthenticationHandler authenticationHandler) throws Exception {
+    super(authenticationManager);
+    setAuthenticationFailureHandler(authenticationHandler);
+    setAuthenticationSuccessHandler(authenticationHandler);
+    //login path
+    setFilterProcessesUrl("/login");
+  }
+
+  private static boolean isContentTypeJson(HttpServletRequest request) {
+    String contentType = request.getContentType();
+    if (!ObjectUtils.isEmpty(contentType)) {
+      return MaximConstant.APPLICATION_JSON_CHARSET_UTF_8.contains(contentType);
+    }
+    // todo
+    throw new RuntimeException();
+  }
+
+  @Override
+  public Authentication attemptAuthentication(HttpServletRequest request,
+      HttpServletResponse response) throws AuthenticationException {
+    if (!HttpMethod.POST.name().equalsIgnoreCase(request.getMethod())) {
+      throw new AuthenticationServiceException(
+          "Authentication method not supported: " + request.getMethod());
+    }
+    String username = null;
+    String password = null;
+    if (isContentTypeJson(request)) {
+      try {
+        Map<String, String> map = OBJECT_MAPPER.readValue(request.getInputStream(),
+            new TypeReference<>() {
+            });
+        username = map.get(getUsernameParameter());
+        password = map.get(getPasswordParameter());
+      } catch (IOException e) {
+        log.error("error occurs while reading username&password from request: {}.", request, e);
+      }
+    } else {
+      username = obtainUsername(request);
+      password = obtainPassword(request);
+    }
+    username = (username != null) ? username.trim() : "";
+    password = (password != null) ? password : "";
+    UsernamePasswordAuthenticationToken authRequest = UsernamePasswordAuthenticationToken.unauthenticated(
+        username, password);
+    // Allow subclasses to set the "details" property
+    setDetails(request, authRequest);
+
+    return this.getAuthenticationManager().authenticate(authRequest);
+  }
+}
