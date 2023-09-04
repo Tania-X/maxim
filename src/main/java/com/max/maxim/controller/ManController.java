2eb09db464ae6ce4c8e304f18727031066ef333a
max <13126565350@163.com>
2023-09-04 15:38:59 +0800

    新增jasypt加解密紧急接口

--- src/main/java/com/max/maxim/controller/ManController.java
+++ src/main/java/com/max/maxim/controller/ManController.java
@@ -5,19 +5,25 @@ import com.max.maxim.bean.vo.ResultEntity;
 import com.max.maxim.enums.ResultEnum;
 import com.max.maxim.util.CaptchaUtil;
 import com.max.maxim.util.ResultUtil;
+import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+import org.jasypt.encryption.StringEncryptor;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("lgn")
+@RequestMapping("man")
 @Slf4j
-public class LoginController {
+public class ManController {
+
+  @Resource
+  private StringEncryptor encryptor;
 
   @GetMapping("isLogin")
   public ResultEntity<DummyEntity> isLogin(HttpServletRequest request) {
@@ -33,6 +39,22 @@ public class LoginController {
   @GetMapping("imageCode")
   public void imageCode(HttpServletRequest request, HttpServletResponse response) {
     CaptchaUtil.createImageCode(request, response);
+  }
+
+  @GetMapping("encrypt")
+  public ResultEntity<String> encrypt(@RequestParam String input) {
+    if (ObjectUtils.isEmpty(input)) {
+      log.warn("input for encryption is empty or null.");
+      return ResultUtil.error(ResultEnum.INPUT_NULL_OR_EMPTY);
+    }
+    return ResultUtil.success(encryptor.encrypt(input));
+  }
+
+  // curl -G --data-urlencode input=eap7SvvM8LpOVfCkRQrzoLe1ortj1EV3W/9Vtd7pFImVfmCN+O7OpA3HWedTqKUY http://localhost:11451/man/decrypt
+  // attention: special characters like '+' should be converted according to ASCII: '+' -> '%2B'
+  @GetMapping("decrypt")
+  public ResultEntity<String> decrypt(@RequestParam String input) {
+    return ResultUtil.success(encryptor.decrypt(input));
   }
 
 }
